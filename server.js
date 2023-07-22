19d52159170400ef3dec07784201357d00881bae
Mona Lisa <130006996+Raquel-t@users.noreply.github.com>
2023-07-22 00:32:57 -0500

    save changes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // Add MySQL password here
+        password: 'Secret.flower1276!',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
